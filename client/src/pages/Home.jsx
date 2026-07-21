22bd23598535bb005d2d5323320efc7a1d711706
Abdullah <mianabdullahyaseen2001@gmail.com>
2026-07-21 16:55:42 +0500

    Offerscards and Home improve

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import FeaturedStore from '../components/FeaturedStore';
+import OfferCards from '../components/OffersCards';
 
 function Home() {
   return (
@@ -8,6 +9,7 @@ function Home() {
       <Navbar />
       <Hero />
       <FeaturedStore />
+      <OfferCards />
     </>
   );
 }
